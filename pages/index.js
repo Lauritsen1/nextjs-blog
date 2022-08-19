cfc7d04037bed49dbebf362690a8ea769b4f218b
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2022-08-19 16:27:15 +0200

    container adjustment

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 			<Header />
 
 			<main>
-				<div className='max-w-7xl mx-auto my-4 space-y-4'>
+				<div className='my-4 space-y-4'>
 					<Post />
 					<Post />
 				</div>
