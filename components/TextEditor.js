9adf534cd255252a15ff30db26b66e3dfc135ccd
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2022-08-27 13:34:58 +0200

    text editor config

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import dynamic from "next/dynamic";
 const ReactQuill = dynamic(import('react-quill'), { ssr: false })
 
@@ -5,10 +6,16 @@ import 'react-quill/dist/quill.snow.css';
 
 export default function TextEditor({ content, setContent }) {
 
+    useEffect(() => {
+        console.log(content);
+
+    }, [content])
+
+
     const toolbarOptions = [
         [{ 'header': 1 }, { 'header': 2 }],
         ['bold', 'italic', 'underline', 'strike'],
-        ['blockquote', 'code-block', 'image', 'link'],
+        ['blockquote', 'image', 'link'],
         [{ 'list': 'ordered' }, { 'list': 'bullet' }],
         [{ 'indent': '-1' }, { 'indent': '+1' }],
         [{ 'color': [] }, { 'background': [] }],
