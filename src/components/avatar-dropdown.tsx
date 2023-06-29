ddda1e6c93e883545fac299cd7f3e94fb71d8953
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-06-29 21:23:59 +0200

    feat: Add avatar component with dropdown menu

---
+++ src/components/avatar-dropdown.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+
+import { User, Settings, LogOut } from 'lucide-react'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu'
+
+export function AvatarDropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarImage src='https://github.com/shadcn.png' />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuItem>
+          <Link href='#' className='flex w-full items-center'>
+            <User className='mr-2 h-4 w-4' />
+            <span>Profile</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href='#' className='flex w-full items-center'>
+            <Settings className='mr-2 h-4 w-4' />
+            <span>Settings</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href='#' className='flex w-full items-center'>
+            <LogOut className='mr-2 h-4 w-4' />
+            <span>Logout</span>
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
