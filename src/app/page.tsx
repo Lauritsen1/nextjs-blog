91f22e74d2142fca12a4f2095192b0112ae49d7e
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-03 21:52:44 +0200

    refactor: Remove unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 import BlogPostCard from '@/components/BlogPostCard'
 
-import { db } from '@/db/index'
-import { post } from '@/db/schema'
-
 export default async function Home() {
-  await db.insert(post).values({
-    title: 'Lorem ipsum',
-    content: 'Teeeeeeeeeeeeeeest',
-  })
-
   return (
     <main className='container py-4'>
       <BlogPostCard />
