7f2ec3e5c5b164ddc0f341c33224a8b479ecdacc
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-04 17:42:53 +0200

    feat: add confirmation dialog for post deletion

--- src/components/DashboardPostCard.tsx
+++ src/components/DashboardPostCard.tsx
@@ -4,6 +4,18 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+
 import { Button } from '@/components/ui/button'
 
 import { Edit, Trash2 } from 'lucide-react'
@@ -23,9 +35,29 @@ export default function DashboardPostCard() {
             <Button variant='ghost' size='icon'>
               <Edit className='h-4 w-4' />
             </Button>
-            <Button variant='destructive' size='icon'>
-              <Trash2 className='h-4 w-4' />
-            </Button>
+
+            <AlertDialog>
+              <AlertDialogTrigger>
+                <Button variant='destructive' size='icon'>
+                  <Trash2 className='h-4 w-4' />
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your post and remove your data from our servers.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction className='bg-destructive text-destructive-foreground'>
+                    Delete
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           </div>
         </div>
       </CardHeader>
