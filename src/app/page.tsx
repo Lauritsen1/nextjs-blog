c0c3a7c4037b406e00c473f63777800c3728a05b
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-06 18:40:49 +0200

    refactor: Update folder structure in app router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import BlogPostCard from '@/components/BlogPostCard'
+import Post from '@/components/mdx/post.mdx'
 
 export default async function Home() {
   return (
     <main className='container py-4'>
       <BlogPostCard />
+      <Post />
     </main>
   )
 }
