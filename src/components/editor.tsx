ad610fe49748bd27659a61f36330fd05f37c6926
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-17 17:02:09 +0200

    feat: add validation to editor component

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -2,14 +2,38 @@
 
 import { useState, useRef, useCallback, useEffect } from 'react'
 
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { useForm } from 'react-hook-form'
+
 import EditorJS from '@editorjs/editorjs'
 import TextareaAutosize from 'react-textarea-autosize'
 
 import '@/styles/editor.css'
 
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(3, {
+      message: 'Title must be at least 3 characters.',
+    })
+    .max(128, {
+      message: 'Title must be less than 128 characters.',
+    })
+    .optional(),
+
+  content: z.any().optional(),
+})
+
 export default function editor() {
   const ref = useRef<EditorJS>()
+  const [isSaving, setIsSaving] = useState<boolean>(false)
   const [isMounted, setIsMounted] = useState<boolean>(false)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
 
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import('@editorjs/editorjs')).default
@@ -34,6 +58,27 @@ export default function editor() {
     }
   }, [])
 
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setIsSaving(true)
+
+    const blocks = await ref.current?.save()
+
+    console.log(data.title)
+
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: data.title,
+        content: blocks,
+      }),
+    })
+
+    setIsSaving(false)
+  }
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setIsMounted(true)
@@ -52,9 +97,11 @@ export default function editor() {
   }, [isMounted, initializeEditor])
 
   return (
-    <form>
+    <form onSubmit={form.handleSubmit(onSubmit)}>
       <div className='prose prose-zinc dark:prose-invert'>
         <TextareaAutosize
+          {...form.register('title')}
+          id='title'
           className='w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none'
           defaultValue='Untitled Post'
           placeholder='Post title'
@@ -62,6 +109,7 @@ export default function editor() {
         />
         <div id='editor'></div>
       </div>
+      <button>save</button>
     </form>
   )
 }
