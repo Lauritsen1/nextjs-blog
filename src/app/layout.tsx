6e154680c29d556742e5f39fd4fd7f207fe99d87
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-17 16:59:08 +0200

    fix: Update import after adjusting file name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-import { ThemeProvider } from '@/components/ThemeProvider'
+import { ThemeProvider } from '@/components/theme-provider'
 
 import SiteHeader from '@/components/site-header'
 
