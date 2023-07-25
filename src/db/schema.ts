e896edbc504922e349344b2c27e9ba0d44db1881
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-25 22:18:40 +0200

    chore: Export db types

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import { mysqlTable, serial, text, varchar, json } from 'drizzle-orm/mysql-core'
+import { type InferModel } from 'drizzle-orm'
 
 export const users = mysqlTable('users', {
   id: varchar('id', { length: 255 }).primaryKey(),
@@ -6,8 +7,12 @@ export const users = mysqlTable('users', {
   imageUrl: text('image_url'),
 })
 
+export type User = InferModel<typeof users>
+
 export const posts = mysqlTable('posts', {
   id: serial('id').primaryKey(),
   title: text('title'),
   content: json('content'),
 })
+
+export type Post = InferModel<typeof posts>
