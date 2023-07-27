21105b8a044d2c358f9badfb33a1f19dd7ec88fc
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-27 02:34:36 +0200

    feat: add initial generation function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const generateInitials = (
+  firstName: string | null | undefined,
+  lastName: string | null | undefined
+) => {
+  const firstInitial = (firstName ?? '').charAt(0).toUpperCase()
+  const lastInitial = (lastName ?? '').charAt(0).toUpperCase()
+
+  return firstInitial + lastInitial
 }
