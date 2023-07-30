f16491c32df7b5b6e7f07c85f645266250f261ee
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-30 19:11:58 +0200

    feat: add header to dashboard page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,6 +8,8 @@ import { usePathname } from 'next/navigation'
 
 import { Newspaper, Settings } from 'lucide-react'
 
+import SiteHeader from '@/components/site-header'
+
 const links = [
   {
     title: 'Posts',
@@ -28,27 +30,31 @@ export default function DashboardLayout({
 }) {
   const pathname = usePathname()
   return (
-    <div className='container grid flex-1 gap-12 py-8 md:grid-cols-[200px_1fr]'>
-      <aside className='hidden flex-col md:flex'>
-        <nav className='grid gap-2'>
-          {links.map((link) => (
-            <Link key={link.title} href={link.href}>
-              <span
-                className={cn(
-                  'group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground',
-                  {
-                    'bg-accent text-accent-foreground': pathname === link.href,
-                  }
-                )}
-              >
-                <link.icon className='mr-2 h-4 w-4' />
-                <span>{link.title}</span>
-              </span>
-            </Link>
-          ))}
-        </nav>
-      </aside>
-      <main>{children}</main>
-    </div>
+    <>
+      <SiteHeader />
+      <div className='container grid flex-1 gap-12 py-8 md:grid-cols-[200px_1fr]'>
+        <aside className='hidden flex-col md:flex'>
+          <nav className='grid gap-2'>
+            {links.map((link) => (
+              <Link key={link.title} href={link.href}>
+                <span
+                  className={cn(
+                    'group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground',
+                    {
+                      'bg-accent text-accent-foreground':
+                        pathname === link.href,
+                    }
+                  )}
+                >
+                  <link.icon className='mr-2 h-4 w-4' />
+                  <span>{link.title}</span>
+                </span>
+              </Link>
+            ))}
+          </nav>
+        </aside>
+        <main>{children}</main>
+      </div>
+    </>
   )
 }
