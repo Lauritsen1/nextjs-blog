fd6b407aa6f66c69d993467030d204c2dfd5ae28
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-31 03:49:55 +0200

    feat: Made webhook route public

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-  publicRoutes: ['/', '/:username/:postId'],
+  publicRoutes: ['/', '/:username/:postId', '/api/webhooks/clerk'],
 })
 
 export const config = {
