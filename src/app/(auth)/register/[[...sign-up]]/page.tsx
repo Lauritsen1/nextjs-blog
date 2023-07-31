4bbfed04ffabca8c650bea2115aca6c95e6bc115
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-31 20:23:08 +0200

    feat: Enhance Sign In page with login instructions

--- src/app/(auth)/register/[[...sign-up]]/page.tsx
+++ src/app/(auth)/register/[[...sign-up]]/page.tsx
@@ -2,7 +2,12 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <main className='flex h-screen items-center justify-center'>
+    <main className='flex h-screen flex-col items-center justify-center'>
+      <p className='mb-2 max-w-sm text-center text-sm'>
+        Create an account or login with, <br /> Email:
+        <span className='font-bold'> demo@example.com</span> and Password:
+        <span className='font-bold'> demo123</span>
+      </p>
       <SignUp />
     </main>
   )
