bf539d4d6bea21d305ab7d49c39bd0ba6bbc72c8
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-31 22:37:19 +0200

    refactor: code formatting

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,16 @@ import { authMiddleware } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 
 export default authMiddleware({
-  publicRoutes: ['/:path*', '/api/webhooks/clerk', '/api/account/update'],
   afterAuth(auth, req, evt) {
     if (!auth.userId && req.nextUrl.pathname.includes('dashboard')) {
       redirect('/')
     }
   },
+  publicRoutes: [
+    '/:username/:postId',
+    '/api/webhooks/clerk',
+    '/api/account/update',
+  ],
 })
 
 export const config = {
