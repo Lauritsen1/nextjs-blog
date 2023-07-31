e82ac5a467638d40943b014cb53f712b3540a056
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-07-31 19:27:53 +0200

    feat: Update Middleware to exclude dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,15 @@
 import { authMiddleware } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 export default authMiddleware({
-  publicRoutes: ['/', '/:username/:postId', '/api/webhooks/clerk'],
+  publicRoutes: ['/:path*', '/api/webhooks/clerk', '/api/account/update'],
+  afterAuth(auth, req, evt) {
+    if (!auth.userId && req.nextUrl.pathname.includes('dashboard')) {
+      const url = req.nextUrl.clone()
+      url.pathname = '/'
+      return NextResponse.redirect(url)
+    }
+  },
 })
 
 export const config = {
