9268e6e60b7ab90c3e6312736329b4fe72b8e6e2
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-08-01 05:58:28 +0200

    Refactor: Simplified authMiddleware routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
 
 export default authMiddleware({
-  afterAuth(auth, req, evt) {
-    if (!auth.userId && req.nextUrl.pathname.includes('dashboard')) {
-      redirect('/')
-    }
-  },
-  publicRoutes: [
-    '/:username/:postId',
-    '/api/webhooks/clerk',
-    '/api/account/update',
-  ],
+  publicRoutes: ['/', '/:postId', '/api/webhooks/clerk', '/api/account/update'],
 })
 
 export const config = {
