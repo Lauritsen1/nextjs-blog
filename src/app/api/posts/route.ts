9e31fdc8cd5a1dd24f9d6fd80648c0797c7f019d
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-08-01 05:25:20 +0200

    Refactor: clean up, add error handling

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,23 +1,23 @@
-import { NextResponse } from 'next/server'
+import { currentUser } from '@clerk/nextjs'
 
 import { db } from '@/db'
-import { posts, Post } from '@/db/schema'
-
-type NewPost = Pick<Post, 'title' | 'content' | 'authorId'>
+import { posts } from '@/db/schema'
 
 export async function POST(request: Request) {
+  const user = await currentUser()
+
   try {
     const body = await request.json()
 
-    const { title, content, authorId } = body as NewPost
-
     const newPost = await db.insert(posts).values({
-      title: title,
-      content: content,
-      authorId: authorId,
+      title: body.title,
+      content: body.content,
+      authorId: user?.id,
     })
 
-    return NextResponse.json(newPost)
+    return new Response(JSON.stringify(newPost), {
+      status: 200,
+    })
   } catch (error: any) {
     return new Response(error.message, {
       status: 400,
