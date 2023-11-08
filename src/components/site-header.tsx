3dba18370a6eea12e744376e4e35f561c86f6ce8
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-11-08 12:06:05 +0100

    Update site header with new title

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -14,10 +14,7 @@ export default async function SiteHeader() {
     <header className='sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur'>
       <div className='container flex h-14 items-center justify-between'>
         <Link href='/'>
-          <span className='flex items-end font-bold'>
-            <Edit3 size={24} />
-            <span>Blog.</span>
-          </span>
+          <span className='font-bold'>Acme Blog</span>
         </Link>
 
         <div className='flex space-x-4'>
